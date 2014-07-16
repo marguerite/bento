67785494e6fdd2c6b5be8b87fcc0996b68537f69
marguerite <i@marguerite.su>
2014-07-16 13:30:33 +0800

    css for submit posts

--- submit_post.php
+++ submit_post.php
@@ -107,38 +107,38 @@ if( isset($_POST['tougao_form']) && $_POST['tougao_form'] == 'send') {
 
 <!-- 关于表单样式，请自行调整-->
 <form class="ludou-tougao" method="post" action="<?php echo $_SERVER["REQUEST_URI"]; $current_user = wp_get_current_user(); ?>">
-    <div style="text-align: left; padding-top: 10px;">
+    <div style="text-align: left; padding-top: 10px; margin-left: 10px;">
         <label for="tougao_authorname">昵称:*</label>
         <input type="text" size="40" value="<?php if ( 0 != $current_user->ID ) echo $current_user->user_login; ?>" id="tougao_authorname" name="tougao_authorname" />
     </div>
 
-    <div style="text-align: left; padding-top: 10px;">
+    <div style="text-align: left; padding-top: 10px; margin-left: 10px;">
         <label for="tougao_authoremail">E-Mail:*</label>
         <input type="text" size="40" value="<?php if ( 0 != $current_user->ID ) echo $current_user->user_email; ?>" id="tougao_authoremail" name="tougao_authoremail" />
     </div>
                    
-    <div style="text-align: left; padding-top: 10px;">
+    <div style="text-align: left; padding-top: 10px; margin-left: 10px;">
         <label for="tougao_authorblog">您的博客:</label>
         <input type="text" size="40" value="<?php if ( 0 != $current_user->ID ) echo $current_user->user_url; ?>" id="tougao_authorblog" name="tougao_authorblog" />
     </div>
 
-    <div style="text-align: left; padding-top: 10px;">
+    <div style="text-align: left; padding-top: 10px; margin-left: 10px;">
         <label for="tougao_title">文章标题:*</label>
         <input type="text" size="40" value="" id="tougao_title" name="tougao_title" />
     </div>
 
-    <div style="text-align: left; padding-top: 10px;">
+    <div style="text-align: left; padding-top: 10px; margin-left: 10px;">
         <label for="tougaocategorg">分类:*</label>
         <?php wp_dropdown_categories('hide_empty=0&id=tougaocategorg&show_count=1&hierarchical=1'); ?>
     </div>
                    
-    <div style="text-align: left; padding-top: 10px;">
+    <div style="text-align: left; padding-top: 10px; margin-left: 10px;">
         <label style="vertical-align:top" for="tougao_content">文章内容:*</label>
         <textarea rows="15" cols="55" id="tougao_content" name="tougao_content"></textarea>
     </div>
                    
     <br clear="all">
-    <div style="text-align: center; padding-top: 10px;">
+    <div style="text-align: left; padding-top: 10px;">
         <input type="hidden" value="send" name="tougao_form" />
         <input type="submit" value="提交" />
         <input type="reset" value="重填" />
